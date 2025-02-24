cdc15921e105d15aa654c2c6425000f21126d2bc
devgaston0712 <devgaston0712@gmail.com>
2025-02-24 11:35:50 -0800

    complete the contact form

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -2,7 +2,29 @@
 @section('meta_title', 'Contact')
 
 @section('content')
-    <div class="container">
-        <contact-form />
-    </div>
+<div class="container">
+    <h1>Contact Us</h1>
+
+    <form action="" method="POST">
+        @csrf
+        <div class="form-group">
+            <label for="name">Your Name</label>
+            <input type="text" class="form-control" id="name" name="name" required>
+        </div>
+
+        <div class="form-group">
+            <label for="email">Your Email</label>
+            <input type="email" class="form-control" id="email" name="email" required>
+        </div>
+
+        <div class="form-group">
+            <label for="message">Your Message</label>
+            <textarea class="form-control" id="message" name="message" rows="4" required></textarea>
+        </div>
+
+        <button type="submit" class="btn btn-primary">Send Message</button>
+    </form>
+</div>
 @endsection
+<!-- resources/views/contact.blade.php -->
+
